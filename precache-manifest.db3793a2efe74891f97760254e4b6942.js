de2569b221a95ea644a483858c3b92a46a54c25f
Shivam <shivampathak12aps@gmail.com>
2022-11-22 21:12:53 +0530

    Updates

---
+++ precache-manifest.db3793a2efe74891f97760254e4b6942.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e07905e846057667ef7",
+    "url": "/todoapplicationassignment/static/css/main.8f70cecf.chunk.css"
+  },
+  {
+    "revision": "5e07905e846057667ef7",
+    "url": "/todoapplicationassignment/static/js/main.5e07905e.chunk.js"
+  },
+  {
+    "revision": "8559a0d314161ffcd7e6",
+    "url": "/todoapplicationassignment/static/js/1.8559a0d3.chunk.js"
+  },
+  {
+    "revision": "f4674bec3b7b6b443bee",
+    "url": "/todoapplicationassignment/static/js/runtime~main.f4674bec.js"
+  },
+  {
+    "revision": "437a6f982616dd1439f48af0e81488b5",
+    "url": "/todoapplicationassignment/index.html"
+  }
+];
